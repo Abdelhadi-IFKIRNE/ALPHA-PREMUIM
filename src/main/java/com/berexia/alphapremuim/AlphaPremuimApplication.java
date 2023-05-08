3bdb7add6d3afe0f4f4e82773fb6a72bf8c2e388
Abdelhadi-IFKIRNE <abdelhadi.ifkirne@gmail.com>
2023-05-08 14:11:51 +0000

    alpha premuim update ...

---
+++ src/main/java/com/berexia/alphapremuim/AlphaPremuimApplication.java
@@ -0,0 +1,13 @@
+package com.berexia.alphapremuim;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlphaPremuimApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AlphaPremuimApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/berexia/alphapremuim/AlphaPremuimApplicationTests.java
@@ -0,0 +1,13 @@
+package com.berexia.alphapremuim;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlphaPremuimApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
